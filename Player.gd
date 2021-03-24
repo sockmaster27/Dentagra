f0f7edd522da43fc9ad6ccc1cf23ca5447e9c2ff
Storvang <61236105+Storvang@users.noreply.github.com>
2021-03-24 18:09:07 +0100

    Player movements Færdig

--- Player.gd
+++ Player.gd
@@ -1,10 +1,9 @@
 extends KinematicBody2D
 
 export var speed: = 300
-var velocity = Vector2()
 
-func calculate_direction():
-	velocity = Vector2()
+func get_movement() -> Vector2:
+	var velocity := Vector2()
 	if Input.is_action_pressed("move_up"):
 		velocity.y -= 1
 	if Input.is_action_pressed('move_down'):
@@ -14,7 +13,9 @@ func calculate_direction():
 	if Input.is_action_pressed('move_right'):
 		velocity.x += 1
 	velocity = velocity.normalized() * speed
+	return velocity
 
 func _physics_process(delta: float) -> void:
-	calculate_direction()
-	velocity = move_and_slide(velocity)
+	var velocity = get_movement()
+	look_at(get_global_mouse_position())
+	move_and_slide(velocity)

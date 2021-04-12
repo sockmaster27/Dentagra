43c39a7344a51e1e59ed194d8e763373fb06befe
sockmaster27 <61235930+sockmaster27@users.noreply.github.com>
2021-04-12 22:31:00 +0200

    Interpolérer positionen (og til dels rotationen)

--- scenes/Enemy.gd
+++ scenes/Enemy.gd
@@ -1,13 +1,32 @@
 class_name Enemy
 extends KinematicBody2D
 
+
 onready var position_tween: Tween = $PositionTween
 onready var rotation_tween: Tween = $RotationTween
 
-func go_to_position(position: Vector2) -> void:
-	#TODO: Interpolér og ekstrapolér
-	set_position(position)
+var expected_delta := 1.0 / Engine.iterations_per_second
 
-func rotate_to_degrees(rotation: float) -> void:
-	#TODO: Interpolér og ekstrapolér
-	set_rotation_degrees(rotation)
+
+func go_to_position(new_position: Vector2) -> void:
+	# TODO: Ekstrapolér
+	position_tween.interpolate_property(
+		self, 
+		"position", 
+		get_position(), 
+		new_position,
+		expected_delta
+	)
+	position_tween.start()
+
+func rotate_to_degrees(new_rotation: float) -> void:
+	# TODO: Ekstrapolér
+	# TODO: Gør så den ikke laver de der vilde 360's (selvom de er ret syge)
+	position_tween.interpolate_property(
+		self, 
+		"rotation_degrees", 
+		get_rotation_degrees(), 
+		new_rotation,
+		expected_delta
+	)
+	position_tween.start()
